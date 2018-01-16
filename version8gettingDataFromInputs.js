766037a1d5b58d9edfb983983d31664b8ebb6cfe
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-01-16 04:09:09 +0000

    changed order or functions

--- version8gettingDataFromInputs.js
+++ version8gettingDataFromInputs.js
@@ -95,9 +95,6 @@ var handlers = {
     displayTodos: function() {
     todoList.displayTodos()
     },
-    toggleAll: function() {
-    todoList.toggleAll();
-    },
     addTodo: function() { 
         var addTodoTextInput = document.getElementById("addTodoTextInput");
         todoList.addTodo(addTodoTextInput.value);
@@ -119,5 +116,8 @@ var handlers = {
         var toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput");
         todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
         toggleCompletedPositionInput.value = "";
+    },
+    toggleAll: function() {
+    todoList.toggleAll();
     }
 };
